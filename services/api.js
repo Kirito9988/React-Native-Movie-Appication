7458ac4ddfd596e725b95ae5debce7dd2f044a77
Kirito9988 <tthakur01@mylangara.ca>
2023-02-03 15:52:19 -0800

    Api.js Commit

--- services/api.js
+++ services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { apiKey, baseUrl } from './config';
 
-export const tmdbService = async (queryType,param,queryParams = '',page = 1
+export const tmdbService = async (queryType,param,queryParams = '',
 ) => {
   
   const url = `${baseUrl}/${queryType}/${param}?api_key=${apiKey}${
     queryParams ? '&query=' + queryParams : ''
-  }&page=${page}`;
+  }`;
  
   const res = await axios.get(url).catch(console.log);
   return res;
